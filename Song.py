8827ab82181230e229f92f63834c76bf6a3daafb
xScoopy <JLeary070@gmail.com>
2021-01-25 19:13:49 -0600

    Finished todos in song.py

--- Song.py
+++ Song.py
@@ -7,29 +7,33 @@ class Song:
 
   # TODO: Create a getter method for the title attribute, called get_title
   def get_title(self):
-    pass
+    '''Getter method for retrieving the title of the song passed in'''
+    return self.__title
   
-  
-  # TODO: Create a setter method for the next_song attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
+  # TODO: Create a setter method for the title attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
   def set_title(self, title):
-    pass
+      '''Method to set the current song title to what's passed in'''
+      self.__title = str(title).title()
+
 
 
   # TODO: Create a getter method for the next_song attribute, called get_next_song
   def get_next_song(self):
-    pass
+    '''Method for retrieving the next_song attribute'''
+    return self.__next_song
 
 
   # TODO: Create a setter method for the next_song attribute, called set_next_song
   def set_next_song(self, next_title):
-    pass
+    '''Method for setting the pointer of the current song to what's passed in'''
+    self.__next_song = str(next_title).title()
 
 
   # TODO: Using the __str___ dunder method, return a string of the song title.
   def __str__(self):
-    pass
+    return str(self.__title).title()
 
 
   # TODO: Using the __repr__ dunder method, return a string formatted as the following:'Song Title -> Next Song Title'
   def __repr__(self):
-    pass
+    return f"{self.get_title()} -> {self.get_next_song()}"
